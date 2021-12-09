34443130f2181501bcf117d24f7473eba5e25733
Parth Zala <parth@technostacks.com>
2021-12-09 12:02:01 +0530

    refactor: simple PWA approach 1 - download button integrate

--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@ window.onload = () => {
         navigator.serviceWorker.register("./sw.js");
     }
 };
+
+const btnDownloadPWA = document.getElementById('btnDownloadPWA')
+let deferredPrompt = null;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+    // Update UI notify the user they can install the PWA
+    btnDownloadPWA.style.display = 'inline-block'
+});
+window.addEventListener("appinstalled", () => {
+    btnDownloadPWA.innerText = "Installed";
+});
+
+async function handleInstallClick() {
+    console.log("deferredPrompt", deferredPrompt);
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+};
